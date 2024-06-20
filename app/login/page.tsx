7d3705414cd57fcf525bb2f42c2465fe053fa050
reymooy27 <gdrrey@gmail.com>
2024-06-20 21:44:29 +0800

    small refine

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,37 @@
 "use client";
+
+import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
+import { useState } from "react";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
+
+  const CALLBACKURL = "http://localhost:3000/";
+
   const handleClick = async () => {
-    await signIn("google", { callbackUrl: "http://localhost:3000/" });
+    setLoading(true);
+    try {
+      await signIn("google", { callbackUrl: CALLBACKURL });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
   };
   return (
-    <button onClick={handleClick} type="submit">
-      Signin with Google
-    </button>
+    <main className="w-full h-screen flex justify-center align-center">
+      <Button
+        disabled={loading}
+        style={{
+          width: "300px",
+          height: "50px",
+          margin: "90px",
+          fontWeight: "bold",
+        }}
+        onClick={handleClick}
+      >
+        {loading ? "Signing In..." : "Signin with Google"}
+      </Button>
+    </main>
   );
 }
